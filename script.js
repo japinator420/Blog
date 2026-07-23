882ecc0720cef8ff69a5435c8b607b1b4920a7ef
japinator420 <amiralvarez678@gmail.com>
2026-07-23 17:55:53 -0500

    Se modifico la presentacion de la seccion peliculas y se corrigieron otros detalles esteticos

--- script.js
+++ script.js
@@ -33,6 +33,40 @@ if(botonAnterior){
     })
 }
 
+// Para los villanos
+let posicionVillanos = 0;
+
+const galeriaVillanos = document.getElementById("galeria-villanos");
+const botonSiguienteVillanos = document.getElementById("boton-siguiente-villanos");
+const botonAnteriorVillanos = document.getElementById("boton-anterior-villanos");
+
+const tarjetasVillanos = document.querySelectorAll('.villano');
+const totalTarjetasVillanos = tarjetasVillanos.length;
+
+function actualizarGaleriaVillanos() {
+    const anchoTarjetaVillano = tarjetasVillanos[0].offsetWidth;
+    console.log(posicionVillanos*anchoTarjetaVillano)
+    galeriaVillanos.style.transform = `translateX(-${posicionVillanos*anchoTarjetaVillano}px)`
+}
+
+if(botonAnteriorVillanos){
+    botonAnteriorVillanos.addEventListener("click", () => {
+        if(posicionVillanos > 0){
+            posicionVillanos--;
+            actualizarGaleriaVillanos();
+        }
+    })
+}
+
+if(botonSiguienteVillanos){
+    botonSiguienteVillanos.addEventListener("click", () => {
+        if(posicionVillanos < totalTarjetasVillanos-1){
+            posicionVillanos++;
+            actualizarGaleriaVillanos();
+        }
+    })
+}
+
 // Variables para las series
 let posicionSeries = 0;
 
@@ -65,6 +99,40 @@ if(botonAnteriorSeries){
         if(posicionSeries>0){
             posicionSeries--;
             actualizarGaleriaSeries();
+        }
+    })
+}
+
+// Para las peliculas
+let posicionPeliculas = 0;
+
+const botonSiguientePeliculas = document.getElementById("boton-siguiente-peliculas")
+const botonAnteriorPeliculas = document.getElementById("boton-anterior-peliculas")
+
+const galeriaPeliculas = document.getElementById("galeria-peliculas")
+const tartejatPeliculas = document.querySelectorAll('.pelicula')
+const totalTarjetasPeliculas = tartejatPeliculas.length
+
+function actualizarGaleriaPeliculas() {
+    const anchoTarjetaPeliculas = tartejatPeliculas[0].offsetWidth;
+    galeriaPeliculas.style.transform = `translateX(-${posicionPeliculas*anchoTarjetaPeliculas}px)`
+}
+
+
+if(botonSiguientePeliculas){
+    botonSiguientePeliculas.addEventListener("click", () =>{
+        if(posicionPeliculas < totalTarjetasPeliculas - 1){
+            posicionPeliculas++;
+            actualizarGaleriaPeliculas();
+        }
+    })
+}
+
+if(botonAnteriorPeliculas){
+    botonAnteriorPeliculas.addEventListener("click", () =>{
+        if(posicionPeliculas > 0){
+            posicionPeliculas--;
+            actualizarGaleriaPeliculas();
         }
     })
 }
